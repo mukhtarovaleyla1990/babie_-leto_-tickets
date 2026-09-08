44455e54cbcef55f6452639a49a7c46a10888d04
mukhtarovaleyla1990 <mukhtarovaleyla1990@gmail.com>
2026-09-08 08:39:15 +0200

    Update activities.ts

--- config/activities.ts
+++ config/activities.ts
@@ -87,7 +87,7 @@ export const ACTIVITIES: Activity[] = [
     id: "yoga",
     name: "Йога",
     time: "09:45 – 11:00",
-    description: "Йога с аидой Чиналиевой. Поющие чаши",
+    description: "Йога с Аидой Чиналиевой. Поющие чаши",
     capacity: 20,
     remainingPlaces: 20,
     isActive: true,
